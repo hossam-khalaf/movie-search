8de042bc1344a3b725122712fa57f98c07514f0d
hossam-khalaf <hossamhassan303089@gmail.com>
2021-08-10 22:25:37 +0200

    API

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,8 +2,25 @@ import React from 'react'
 import './SearchMovies.css'
 
 const SearchMovies = () => {
+	const searchMovies = async (e) => {
+		e.preventDefault()
+
+		const query = ''
+
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=1fe844e090d2aab7a7976a3cbfa5d31b&language=en-US&query=${query}&page=1&include_adult=false`
+
+		try {
+			const res = await fetch(url)
+			const data = await res.json()
+
+			console.log(data)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
 	return (
-		<form className='form'>
+		<form className='form' onSubmit={searchMovies}>
 			<label htmlFor='query' className='label'>
 				Movie Name:
 			</label>
